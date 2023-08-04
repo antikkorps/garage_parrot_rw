d59eafc339f76355eef87ec2869c84dd44d84671
Franck <franckvienot7@gmail.com>
2023-08-04 23:17:49 +0200

    added display of AnnoncesCell on homepage

---
+++ web/src/components/AnnoncesVehiculesCell/AnnoncesVehiculesCell.jsx
@@ -0,0 +1,45 @@
+export const QUERY = gql`
+  query AnnoncesVehiculesQuery {
+    annonces {
+      id
+      title
+      description
+      kilometrage
+      yearofcirculation
+      price
+      published
+      authorId
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ annonces }) => {
+  return (
+    <ul>
+      {annonces.map((annonce) => {
+        return (
+          <article key={annonce.id}>
+            <h2>{annonce.title}</h2>
+            <p>Description : {annonce.description}</p>
+            <p>Kilométrage : {annonce.kilometrage}</p>
+            <p>Année de mise en circulation : {annonce.yearofcirculation}</p>
+            <p>Prix : {annonce.price} Euros</p>
+            <p>{annonce.published}</p>
+            <p>
+              {annonce.createdAt} by {annonce.authorId}
+            </p>
+          </article>
+        )
+      })}
+    </ul>
+  )
+}
--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import { MetaTags } from '@redwoodjs/web'
 
+import AnnoncesVehiculesCell from 'src/components/AnnoncesVehiculesCell'
+
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <main>Home</main>
+      <AnnoncesVehiculesCell />
     </>
   )
 }
